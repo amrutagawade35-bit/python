ef298199f1b090a5df72e5b65479733b0c390ac8
Amruta Gawade <amrutagawade35@gmail.com>
2026-06-04 14:52:17 +0530

    first python code

---
+++ hello.py
@@ -0,0 +1,11 @@
+"""def hello():
+    print("Hello")
+hello()
+hello()"""
+
+def judge():
+    if 2>4:
+        print("2 is greater than 4")
+    else:
+        print("2 is not greater than 4")
+judge()
